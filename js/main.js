5b1348e90af22204089d4878c8a681be152d4d61
lcwyl <87659128+lcwyl@users.noreply.github.com>
2021-08-10 06:40:55 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-
 let arr = [1,2,3,4,5,6]
-arr.forEach(function(el){
+function oblicz(tablica){
+tablica.forEach(function(el){
+   console.log('mnozenie')
    console.log(el * el);
-});
-
-arr.forEach(function(el){
+   console.log('dodawanie');
    console.log(el + el);
 });
-
-
+}
+oblicz(arr);
 
 
 
