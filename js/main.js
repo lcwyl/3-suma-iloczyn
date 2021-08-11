7dbc16a481121628515ed9a98380f4ff02dda1db
lcwyl <87659128+lcwyl@users.noreply.github.com>
2021-08-11 18:10:40 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-let arr = [1,2,3,4,5,6]
 function oblicz(tablica){
+let suma = 0;
+let iloczyn = 1;
 tablica.forEach(function(el){
-   console.log('mnozenie')
-   console.log(el * el);
-   console.log('dodawanie');
-   console.log(el + el);
+   suma +=el;
+   iloczyn*=el;
 });
+console.log(suma);
+console.log(iloczyn);
+
 }
 oblicz(arr);
+
+
+
 
 
 
